b41376f97576ae246ee90a9f10957b5b6879c6cb
Kent <117391729+Kentypa@users.noreply.github.com>
2025-01-04 00:07:05 +0200

    Fixed problem with keys

--- src/components/Main Cabinet/index.tsx
+++ src/components/Main Cabinet/index.tsx
@@ -15,8 +15,8 @@ export const MainCabinet: React.FC<MainCabinetProps> = ({user}) => {
             <h1>Main Cabinet</h1>
             <ul>
                 <li key={1}>user id: {user.id}</li>
-                <li key={1}>user email: {user.email}</li>
-                <li key={1}>user password: {user.password}</li>
+                <li key={2}>user email: {user.email}</li>
+                <li key={3}>user password: {user.password}</li>
             </ul>
         </div>
     )
